0ec51fda1e1a32ed52139f6aa3bd6828e39acbf9
Husainhusaini <90894179+Husainhusaini@users.noreply.github.com>
2021-09-29 16:44:45 +0700

    tugas praktikum 1

---
+++ tugas 1.cpp
@@ -0,0 +1,5 @@
+﻿Build started...
+1>------ Build started: Project: tugas 1, Configuration: Debug Win32 ------
+1>tugas 1.cpp
+1>tugas 1.vcxproj -> C:\Users\ALMUHDOR\source\repos\tugas 1\Debug\tugas 1.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
